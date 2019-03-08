4b4c3fc20e3e5007ffe37fc9cadbe8102e1c3c6c
Camila L <camila.leite.oliveira@gmail.com>
2019-03-08 20:01:18 -0300

    prod

--- web/footer.php
+++ web/footer.php
@@ -2,5 +2,6 @@
 			<div class="content has-text-centered"><?php echo FOOTER; ?></div>
 		</footer>
 		<script src="web/assets/js/bulma.js"></script>
+		<script src="web/assets/js/messenger.js"></script>
 	</body>
 </html>
