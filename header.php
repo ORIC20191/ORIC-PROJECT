a08f6f4b3dfe6fd443ff5dd02a6f4a425b4e652c
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2019-03-29 02:04:39 -0300

    Update header.php

--- header.php
+++ header.php
@@ -7,8 +7,12 @@
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <title><?php echo $title; ?></title>
-	    <link rel="shortcut icon" href="web/assets/brand/2_crop.png" type="image/x-icon">
+	    <link rel="shortcut icon" href="assets/brand/2_crop.png" type="image/x-icon">
 	    <!-- Bulma Version 0.7.4-->
-	    <link rel="stylesheet" href="web/assets/css/style.css" />
+	    <link rel="stylesheet" href="assets/css/style.css" />
+	    <link type="text/css" href="assets/bulma-pageloader/dist/css/bulma-pageloader.min.css" rel="stylesheet">
+        <link type="text/css" href="assets/bulma-pageloader/src/sass/index.sass" rel="stylesheet">
+        <!-- JS -->
+        <script src="assets/js/menu.js"></script>
 	</head>
 	<body>
