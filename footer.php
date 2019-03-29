a1dc3a03378c2762708f10b959d3932cfa41591a
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2019-03-29 02:04:43 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,5 @@
 		<footer class="footer" id="contato">
 			<div class="content has-text-centered"><?php echo FOOTER; ?></div>
 		</footer>
-		<script src="web/assets/js/bulma.js"></script>
-		<script src="web/assets/js/messenger.js"></script>
 	</body>
 </html>
