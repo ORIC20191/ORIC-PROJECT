7e44609929b7e71c286d8aecc4acb553203aa12d
Camila L <camila.leite.oliveira@gmail.com>
2019-05-08 19:45:10 -0300

    Import Django, PIP, Virtualenv
    Create a Python Project and import the seetings of the formest PHP Project

--- assets/js/print.js
+++
@@ -1,8 +0,0 @@
-	document.getElementById('btn').onclick = function() {
-            var conteudo = document.getElementById('print').innerHTML,
-                tela_impressao = window.open('about:blank');
-
-            tela_impressao.document.write(conteudo);
-            tela_impressao.window.print();
-            tela_impressao.window.close();
-        };
--- footer.php
+++
@@ -1,5 +0,0 @@
-		<footer class="footer" id="contato">
-			<div class="content has-text-centered"><?php echo FOOTER; ?></div>
-		</footer>
-	</body>
-</html>
