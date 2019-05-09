04fd6cc069bee28ffdaeec9c3a205dccf8420c47
Camila L <camila.leite.oliveira@gmail.com>
2019-05-08 21:22:32 -0300

    Import the assets from old version

---
+++ web/assets/js/print.js
@@ -0,0 +1,8 @@
+	document.getElementById('btn').onclick = function() {
+            var conteudo = document.getElementById('print').innerHTML,
+                tela_impressao = window.open('about:blank');
+
+            tela_impressao.document.write(conteudo);
+            tela_impressao.window.print();
+            tela_impressao.window.close();
+        };
