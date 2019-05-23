5f392f038abd5dc27488961c5369e67a32cd60b4
Camila L <camila.leite.oliveira@gmail.com>
2019-05-23 19:39:45 -0300

    Update settings.py

--- web/web/settings.py
+++ web/web/settings.py
@@ -103,7 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#definindo idioma para português do Brasil e seu respectivo fuso horário
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
